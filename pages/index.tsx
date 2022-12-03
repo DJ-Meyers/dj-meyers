497e85c3d1ea699086f95f0a324101d20d1fe91d
DJ Meyers <djmeyers@outlook.com>
2022-12-02 21:14:13 -0600

    Change public link for pdf

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,9 @@ export default function Home() {
           <div className='basis-1/2 flex flex-col pt-10 pl-2'>
             <a target='_blank' rel='noreferrer' href='https://cfbguessr.com' className='text-xl leading-6 mb-2 text-slate-300 hover:text-white'>CFBGuessr</a>
             <a target='_blank' rel='noreferrer' href='https://dj-meyers.github.io/gloomhaven-modifier-calculator/' className='text-xl leading-6 mb-2 text-slate-300 hover:text-white'>Gloomhaven Calculator</a>
-            <a target='_blank' rel='noreferrer' href='https://docs.google.com/document/d/e/2PACX-1vS0tqRT2zvzhRV5z4kArwAhIxc4SSp9XsunDIzw3e52HRTShvsTgigGWZvY8NL1q7HKrEa28h730oTP/pub' className='text-xl leading-6 mb-2 text-slate-300 hover:text-white'>Resume</a>
-            <a target='_blank' rel='noreferrer' href='https://github.com/dj-meyers' className='text-xl leading-6 mb-2 text-slate-300 hover:text-white'>Github</a>
+            <a target='_blank' rel='noreferrer' href='https://drive.google.com/file/d/14oelwmrGSAF1ZV0IZh9eCDa7x7X9_zXC/view' className='text-xl leading-6 mb-2 text-slate-300 hover:text-white'>Resume</a>
             <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/dj-meyers/' className='text-xl leading-6 mb-2 text-slate-300 hover:text-white'>LinkedIn</a>
+            <a target='_blank' rel='noreferrer' href='https://github.com/dj-meyers' className='text-xl leading-6 mb-2 text-slate-300 hover:text-white'>Github</a>
             </div>
         </div>
         <h3 className='mt-auto mb-12 text-center'>To get in touch, email [me at djmeyers dot com]</h3>
